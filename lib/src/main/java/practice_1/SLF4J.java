f670815746a1d8d28a6958aa1c6a7ee1db877f1b
chen84928 <chen84928@DESKTOP-SDNF9DJ.cdsafi.com>
2022-04-19 09:10:47 +0800

    first

---
+++ lib/src/main/java/practice_1/SLF4J.java
@@ -0,0 +1,17 @@
+package practice_1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class SLF4J {
+private static Logger LOGGER = LoggerFactory.getLogger(SLF4J.class);
+
+
+
+public static void main(String[] args) {
+LOGGER.info("Hello World");
+//System.out.println("Hello World");
+}
+}
